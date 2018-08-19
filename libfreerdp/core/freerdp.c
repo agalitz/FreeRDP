6c68672d6626ba3bf175a01e1682dc1490c0cc78
root <root@localhost.localdomain>
2018-08-19 15:31:15 +0300

    break connection

--- libfreerdp/core/freerdp.c
+++ libfreerdp/core/freerdp.c
@@ -153,9 +153,7 @@ BOOL freerdp_connect(freerdp* instance)
 	rdpSettings* settings;
 	ConnectionResultEventArgs e;
 
-	tttt
-
-	if (!instance)
+	if (true || !instance)
 		return FALSE;
 
 	/* We always set the return code to 0 before we start the connect sequence*/
